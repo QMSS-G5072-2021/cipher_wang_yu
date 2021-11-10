5848e29c50f047710d2466c30d28c33aeb350117
nicolewang97 <90565190+nicolewang97@users.noreply.github.com>
2021-11-10 18:35:39 -0500

    update

--- tests/test_cipher_yw3760.py
+++ tests/test_cipher_yw3760.py
@@ -3,5 +3,5 @@ from cipher_yw3760 import cipher_yw3760
 def test_cipher_singleword():
     example = 'Ceasar'
     expected = 'Dfbtbs'
-    result = cipher(example, 1, True)
+    result = cipher_yw3760.cipher(example, 1, True)
     assert result == expected
